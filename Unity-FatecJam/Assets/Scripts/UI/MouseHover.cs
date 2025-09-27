6df0c0d30da9ec19694f843da67be59599fd9460
Renan Granzotti <renanpgranzotti@gmail.com>
2025-09-27 08:38:07 -0300

    Adição de UI

--- Unity-FatecJam/Assets/Scripts/UI/MouseHover.cs
+++ Unity-FatecJam/Assets/Scripts/UI/MouseHover.cs
@@ -11,12 +11,22 @@ public class MouseHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
     [SerializeField] private AudioClip hoverAudioClip;
     [SerializeField] private AudioClip clickAudioClip;
 
+    [Header("Interação com Sprite (Reguer Image)")]
+    [SerializeField] private Sprite hoverSprite;
+    private Sprite initialSprite;
+
     private AudioSource _audioSource;
+    private SpriteRenderer _spriteRenderer;
 
     public void Start()
     {
         defaultScale = transform.localScale.x;
         _audioSource = GetComponent<AudioSource>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        if (_spriteRenderer != null && hoverSprite != null)
+        {
+            initialSprite = _spriteRenderer.sprite;
+        }
     }
 
     public void OnPointerDown(PointerEventData eventData)
@@ -42,6 +52,11 @@ public class MouseHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
                 RandomizeAudioSettings();
                 _audioSource.PlayOneShot(hoverAudioClip);
             }
+
+            if (_spriteRenderer != null && hoverSprite != null)
+            {
+                _spriteRenderer.sprite = hoverSprite;
+            } 
         }
     }
 
@@ -50,6 +65,10 @@ public class MouseHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
         if (gameObject.activeInHierarchy)
         {
             transform.localScale = Vector2.one * defaultScale;
+            if (_spriteRenderer != null && hoverSprite != null)
+            {
+                _spriteRenderer.sprite = initialSprite;
+            }
         }
     }
 
